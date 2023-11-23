f12cdfedc46a46fcc0aec008c6c8acf205edfa8e
Dallison Lima <dallisonlima@gmail.com>
2023-11-22 21:23:08 -0300

    Ajustes no views.py e no inbox.js

--- mail/views.py
+++ mail/views.py
@@ -151,6 +151,8 @@ def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("index"))
 
+MAILREGISTER = "mail/register.html"
+
 
 def register(request):
     if request.method == "POST":
@@ -160,7 +162,7 @@ def register(request):
         password = request.POST["password"]
         confirmation = request.POST["confirmation"]
         if password != confirmation:
-            return render(request, "mail/register.html", {
+            return render(request, MAILREGISTER, {
                 "message": "Passwords must match."
             })
 
@@ -170,10 +172,10 @@ def register(request):
             user.save()
         except IntegrityError as e:
             print(e)
-            return render(request, "mail/register.html", {
+            return render(request, MAILREGISTER, {
                 "message": "Email address already taken."
             })
         login(request, user)
         return HttpResponseRedirect(reverse("index"))
     else:
-        return render(request, "mail/register.html")
+        return render(request, MAILREGISTER)
